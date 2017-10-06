d1a054d1a4969d52745fc86bd665a894649a83d7
Mr. Fleming <efleming@stpatrick.org>
2017-10-06 12:01:11 -0500

    incorporated mergesort, still need to do quicksort

--- Sort.js
+++ Sort.js
@@ -32,7 +32,46 @@ const Sort = {
             }
         }
         return list;
-    }
+    },
+    merge: function(thelist){
+        function mergesort(list){
+            if(list.length <2){
+                return list;
+            }
+            let mid = Math.floor(list.length/2);
+            let left = list.slice(0,mid);
+            let right = list.slice(mid,list.length);
 
+            return combine(mergesort(left),mergesort(right));
+        }
+        function combine(left,right){
+            let result = [];
+            let x=0,y=0;
+            let xlen =left.length;
+            let ylen =right.length;
+
+            while(xlen > x && ylen > y){
+                if(left[x] <= right[y]){
+                    result[x+y]= left[x];
+                    x++;
+                }
+                else{
+                    result[x+y] = right[y];
+                    y++;
+                }
+            }
+            while(xlen > x){
+                result[x+y]= left[x];
+                x++;
+            }
+            while(ylen > y){
+                result[x+y] = right[y];
+                    y++;
+
+            }
+            return result;
+        }
+        return mergesort(thelist);
+    }
 }
 module.exports = Sort;
